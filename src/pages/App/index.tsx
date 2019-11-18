fffcb286b5ee2bcdd35508597b31613c18dd7938
Yago Arroyo <yagoag@gmail.com>
2019-11-18 15:07:57 -0300

    Initial react project

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <header className="header">
+        <h1>The Movie Catalog</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
